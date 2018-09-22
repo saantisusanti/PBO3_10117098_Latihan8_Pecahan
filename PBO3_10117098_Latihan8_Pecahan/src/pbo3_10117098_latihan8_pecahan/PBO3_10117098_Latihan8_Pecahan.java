cf306540f62b359df8f190bf7e42b5d0224f4c31
Santi Susanti <43363256+saantisusanti@users.noreply.github.com>
2018-09-23 00:16:54 +0700

    Add files via upload

---
+++ PBO3_10117098_Latihan8_Pecahan/src/pbo3_10117098_latihan8_pecahan/PBO3_10117098_Latihan8_Pecahan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117098_latihan8_pecahan;
+
+/**
+ *
+ * @author 
+ * NAMA  : Santi Susanti
+ * KELAS : PBO3
+ * NIM   : 10117098
+ */
+public class PBO3_10117098_Latihan8_Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float\t : " + f);
+        System.out.println("Double\t : " + d);
+    }
+    
+}
